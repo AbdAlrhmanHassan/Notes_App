5ac4fbe05dffc7fadcf66faea75c2141a317bcd3
AbdAlrhman Hassan <aalrhman675@gmail.com>
2023-06-12 02:32:48 +0300

    add fonts

--- lib/views/NotePage.dart
+++ lib/views/NotePage.dart
@@ -5,6 +5,41 @@ class NotePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Notes",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22),
+                ),
+                CustomSearchIcon()
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: const Color.fromARGB(67, 158, 158, 158),
+        ),
+        child: IconButton(
+            onPressed: () {}, icon: const Icon(Icons.search)));
   }
 }
