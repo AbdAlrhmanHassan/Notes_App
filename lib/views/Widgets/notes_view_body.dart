818b270c10650b6e21d8c706322b5bddfdd27ca0
AbdAlrhman Hassan <aalrhman675@gmail.com>
2023-06-12 17:09:23 +0300

    LL

--- lib/views/Widgets/notes_view_body.dart
+++ lib/views/Widgets/notes_view_body.dart
@@ -13,9 +13,9 @@ class NotesViewBody extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: const Text("Notes"),
+        title: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text("Notes"),
         ),
         actions: const [CustomSearchIcon()],
       ),
@@ -37,37 +37,42 @@ class NoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(24),
       width: double.infinity,
       decoration: BoxDecoration(
-        color: Colors.orange[300],
+        color: Color(0xffffcd7a),
         borderRadius: BorderRadius.circular(20),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-          ListTile(
-            title: const Text(
-              "Flutter tips",
-              style: TextStyle(color: Colors.black, fontSize: 24),
-            ),
-            subtitle: const Text(
-              "Here you can write your notes . ",
-              style: TextStyle(color: Colors.black, fontSize: 18),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 26,
-                )),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        ListTile(
+          minVerticalPadding: 0,
+          contentPadding: EdgeInsets.all(0),
+          title: const Text(
+            "Flutter tips",
+            style: TextStyle(color: Colors.black, fontSize: 24),
           ),
-          const Text(
-            "June 11,2023",
-            style: TextStyle(color: Colors.black, fontSize: 18),
-          )
-        ]),
-      ),
+          subtitle: Text(
+            "Here you can write your notes . ",
+            style: TextStyle(
+                color: Colors.black.withOpacity(.5),
+                fontSize: 20,
+                fontWeight: FontWeight.w500),
+          ),
+          trailing: IconButton(
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+            icon: const Icon(
+              Icons.delete,
+              color: Colors.black,
+              size: 26,
+            ),
+          ),
+        ),
+        const Text(
+          "June 11,2023",
+          style: TextStyle(color: Colors.black, fontSize: 18),
+        )
+      ]),
     );
   }
 }
